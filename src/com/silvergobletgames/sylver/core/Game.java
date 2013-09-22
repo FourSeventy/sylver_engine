0ea0b30912e7b70d6967a140f5770cda4b8be0b0
Michael Signorella <michael@silvergobletgames.com>
2013-09-22 16:29:44 -0400

    fixing up game event qeue system

--- src/com/silvergobletgames/sylver/core/Game.java
+++ src/com/silvergobletgames/sylver/core/Game.java
@@ -3,15 +3,12 @@ package com.silvergobletgames.sylver.core;
 
 import com.silvergobletgames.sylver.audio.AudioRenderer;
 import com.silvergobletgames.sylver.graphics.OpenGLGameWindow;
-import com.silvergobletgames.sylver.graphics.ShaderLoader;
-import java.awt.Point;
 import java.io.StringWriter;
 import java.io.PrintWriter;
 import java.net.URI;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -36,7 +33,10 @@ public final class Game
     private ConcurrentHashMap<String,Runnable> runnableMap = new ConcurrentHashMap<>();
 
     //event queue for scene changes etc
-    private ConcurrentLinkedQueue<SceneAction> sceneActionQueue = new ConcurrentLinkedQueue<>()
+    private ConcurrentLinkedQueue<SceneAction> sceneActionQueue = new ConcurrentLinkedQueue<>();
+    
+    //system exit actions
+    private ArrayList<SystemExitAction> systemExitActions = new ArrayList<>();
        
     //asset manager
     private AssetManager assetManager;
@@ -47,8 +47,7 @@ public final class Game
     //input handler
     private InputHandler inputHandler;
 
-    //system exit actions
-    private ArrayList<SystemExitAction> systemExitActions = new ArrayList<>();
+    
     
     //game running variable
     private boolean gameRunning = true;
@@ -166,10 +165,14 @@ public final class Game
 
                 //save the start of the loop time
                 startOfLoopTime = System.nanoTime();
-
-                //aquire lock on current scene
-                this.currentSceneLock.lock();
-
+                
+                //proccess scene actions
+                while(!this.sceneActionQueue.isEmpty())
+                {
+                    SceneAction sceneAction = this.sceneActionQueue.poll();
+                    sceneAction.action();
+                    
+                }
 
                 //if we have a scene to update
                 if(currentScene != null)
@@ -212,8 +215,6 @@ public final class Game
                     //=================================
                     graphicsWindow.renderScene(scene); 
                 }
-                //unlock current scene
-                this.currentSceneLock.unlock();
 
                 //note the current time
                 endOfLoopTime = System.nanoTime();
@@ -272,16 +273,40 @@ public final class Game
      */
     public void changeScene(final Class scene,final ArrayList args)
     {
-       SceneAction action = new SceneAction(ActionEnum.CHANGESCENE,scene);
+       SceneAction action = new SceneAction(SceneAction.ActionEnum.CHANGESCENE){
+           
+           public void action()
+           {
+               
+                //exit old scene
+                if (currentScene != null)
+                    scenes.get(currentScene).sceneExited();
+
+                //Enter the new scene
+                currentScene = scene;
+                scenes.get(currentScene).sceneEntered(args);
+
+                //reset accumulator
+                accumulator = 0;
+               
+           }
+       };
+       this.sceneActionQueue.add(action);
+       
     }  
     
     /**
      * Loads a scene into memory. A scene must be loaded before it can become active
      * @param scene The scene to load
      */
-    public void loadScene(Scene scene) 
+    public void loadScene(final Scene scene) 
     {
-       
+     
+        if (scenes.containsKey(scene.getClass()))
+                 throw new RuntimeException("Scene map already contains this scene");
+             else
+                 scenes.put(scene.getClass(), scene);
+
     }
     
     /**
@@ -291,36 +316,20 @@ public final class Game
      */
     public void unloadScene(final Class scene) 
     {
-        //if its not the current scene, unload it immediately
-        if(scene != this.currentScene)
-        {
-            scenes.remove(scene); 
-        }
-        else if(scene == currentScene) //if we are unloading the current scene
-        {
-            
-             Thread thread = new Thread()
-            {           
-                @Override
-                public void run()
-                {
-                    //try to aquire lock on current scene
-                    //block until we obtain lock
-                    currentSceneLock.lock();  
-                    try 
-                    {
-                     
-                    } 
-                    finally 
-                    {
-                        currentSceneLock.unlock();
-                    }
+       
+        SceneAction action = new SceneAction(SceneAction.ActionEnum.CHANGESCENE){
+           
+           public void action()
+           {
+               //remove scene
+                scenes.remove(scene);
 
-                }
-            };
-            thread.start();
-            
-        }
+                //set current scene to null if it is equal to the scene we unloaded
+                if (scene == currentScene)
+                    currentScene = null;
+           }
+       };
+        this.sceneActionQueue.add(action);
         
            
     }
@@ -402,59 +411,6 @@ public final class Game
     public void removeRunnable(String key)
     {
         this.runnableMap.remove(key);
-    }
-
-
-    private void processSceneActionQueue()
-    {
-    	for(SceneAction sceneAction: this.SceneActionQueue)
-    	{
-    		ActionEnum action = sceneAction.action;
-    		Class scene = sceneAction.scene;
-    		Arraylist args = sceneAction.args;
-
-
-    		switch(action)
-    		{
-    			case CHANGESCENE:
-    			{
-    				 //exit old scene
-                    if (currentScene != null)
-                        scenes.get(currentScene).sceneExited();
-
-                    //Enter the new scene
-                    currentScene = scene;        
-                    scenes.get(currentScene).sceneEntered(args);
-                    
-                    //reset accumulator
-                    accumulator = 0;
-
-    			}
-    			break;
-
-    			case LOADSCENE:
-    			{
-    				if(scenes.containsKey(scene.getClass()))
-			            throw new RuntimeException("Scene map already contains this scene");
-			        else
-			           this.scenes.put(scene.getClass(),scene);
-
-    			}
-    			break;
-
-    			case UNLOADSCENE:
-    			{    				
-                   //remove scene
-				   scenes.remove(scene);
-                   
-                   //set current scene to null if it is equal to the scene we unloaded
-                   if(scene == currentScene)
-                       currentScene = null;
-
-    			}
-    			break;
-    		}
-    	}
     }
     
     
@@ -617,32 +573,24 @@ public final class Game
     * Class used to hold game actions that will be performed by Game
     *
     */
-    private static class SceneAction
+    private static abstract class SceneAction
     {
     	protected ActionEnum action;
-    	protected Class scene;
-    	protected ArrayList args;
 
 
     	//private enum used for
-	    private enum ActionEnum
-	    {
-	    	CHANGESCENE,LOADSCENE,UNLOADSCENE;
-	    }
+        private static enum ActionEnum
+        {
+            CHANGESCENE,UNLOADSCENE;
+        }
+        
+        protected SceneAction(ActionEnum en)
+        {
+            this.action = en;
+        }
 
-    	public SceneAction(ActionEnum action, Class scene)
-    	{
-    		this.action = action;
-    		this.scene = scene;
-
-    	}
-
-    	public SceneAction(ActionEnum action, Class scene, ArrayList args)
-    	{
-    		this(action,scene);
-    		args = args;
-    	}
-
+    	public abstract void action();
+     
     }
         
 }
